08d67b7dc3f6c95b0e32b8e817eb7b5eea56b13b
daehwan0307 <eoghks960307@naver.com>
2022-09-27 20:36:49 +0900

    버블정열 알고리즘

--- src/main/java/bubblesort/BubbleSort.java
+++ src/main/java/bubblesort/BubbleSort.java
@@ -1,7 +1,29 @@
 package bubblesort;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public static int[] bubbleSort(int n, int[] arr){
+        for(int i = 0; i < n; i++) {
+            for(int j = 0 ; j < n - i - 1 ; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
+            int[] a = {5,1,3,8,2,7,6};
+
+            System.out.println(Arrays.toString(a));
+            bubbleSort(7,a);
+        System.out.println(Arrays.toString(a));
 
     }
+
 }
