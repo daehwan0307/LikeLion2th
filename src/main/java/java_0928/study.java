e040b566ffbd16cbe65f74be5a69a327ac62a586
daehwan0307 <eoghks960307@naver.com>
2022-09-28 14:24:09 +0900

    try catch 문 예제

---
+++ src/main/java/java_0928/study.java
@@ -0,0 +1,28 @@
+package java_0928;
+
+import java.util.Scanner;
+
+public class study {
+
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        try {
+            System.out.print("a/b... a?");
+            int n1 = kb.nextInt();
+
+            System.out.print("a/b ... b?");
+            int n2 = kb.nextInt();
+            System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+        }catch (ArithmeticException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("무조건 실행");
+        }
+
+        System.out.println("good bye");
+    }
+}
