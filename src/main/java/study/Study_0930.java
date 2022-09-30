094623da04bbd78dc8eb7c3bc0e448e1482eae1e
daehwan0307 <eoghks960307@naver.com>
2022-09-30 11:18:07 +0900

    calendar class study

--- src/main/java/study/Study_0930.java
+++ src/main/java/study/Study_0930.java
@@ -1,10 +1,36 @@
 package study;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Study_0930 {
     public static void main(String[] args) {
-        
+
+        Calendar d = Calendar.getInstance();
+
+
+        /*
+        int year = d.get(Calendar.YEAR);
+        int month = d.get(Calendar.MONTH);
+        int date = d.get(Calendar.DATE);
+        int hour12 = d.get(Calendar.HOUR);
+        int hour24 = d.get(Calendar.HOUR_OF_DAY);
+        int min = d.get(Calendar.MINUTE);
+        int sec = d.get(Calendar.SECOND);
+        System.out.println(year);
+        System.out.println(month);
+        System.out.println(date);
+        System.out.println(hour12);
+        System.out.println(hour24);
+        System.out.println(min);
+        System.out.println(sec);
+
+*/
+
+
+
+
+        /*
         Scanner sc = new Scanner(System.in);
         System.out.println("문자열을 입력하세요. 빈칸이 있어도 되고 영어 한글 모두 됩니다.");
         String str = new String();
@@ -18,7 +44,7 @@ public class Study_0930 {
 
 
 
-        /*
+
         StringBuilder sb = new StringBuilder("123");
         sb.append(4567);
         sb.delete(0,2);
