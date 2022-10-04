3f724cf4f410014e537f7068fccb38b85107458e
daehwan0307 <eoghks960307@naver.com>
2022-10-04 16:20:11 +0900

    .

---
+++ src/main/java/EvenOdd2/Main.java
@@ -0,0 +1,17 @@
+package EvenOdd2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b;
+
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+
+
+
+    }
+}
---
+++ src/main/java/codeup/Main.java
@@ -0,0 +1,13 @@
+package codeup;
+//code up 1010번 문제
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
