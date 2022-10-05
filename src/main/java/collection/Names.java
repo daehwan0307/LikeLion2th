b9ad860829dd8e6843bbe29c446aa99c6a38e8b6
daehwan0307 <eoghks960307@naver.com>
2022-10-05 20:54:49 +0900

    students name 출력

---
+++ src/main/java/collection/Names.java
@@ -0,0 +1,116 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Names {
+    private List<String> students = new ArrayList<>();
+
+    public List<String> names(){
+
+        this.students.add("권하준");
+                this.students.add("조성윤");
+                this.students.add("안예은");
+                this.students.add("남우빈");
+                this.students.add("최경민");
+                this.students.add("안준휘");
+                this.students.add("하채민");
+                this.students.add("허도한");
+                this.students.add("배태훈");
+                this.students.add("정해성");
+                this.students.add("하찬희");
+                this.students.add("박도아");
+                this.students.add("유경준");
+                this.students.add("고다니");
+                this.students.add("하이안");
+                this.students.add("고유호");
+                this.students.add("전민찬");
+                this.students.add("배강");
+                this.students.add("유서이");
+                this.students.add("황도빈");
+                this.students.add("강규진");
+                this.students.add("임예린");
+                this.students.add("양예온");
+                this.students.add("곽윤재");
+                this.students.add("한단");
+                this.students.add("박강현");
+                this.students.add("윤성민");
+                this.students.add("박재윤");
+                this.students.add("서현준");
+                this.students.add("노유주");
+                this.students.add("전율");
+                this.students.add("노아린");
+                this.students.add("권아윤");
+                this.students.add("송태서");
+                this.students.add("정라온");
+                this.students.add("서서한");
+                this.students.add("문주호");
+                this.students.add("오세아");
+                this.students.add("이호영");
+                this.students.add("손윤아");
+                this.students.add("이동우");
+                this.students.add("남혜준");
+                this.students.add("황솔 ");
+                this.students.add("권은재");
+                this.students.add("곽준휘");
+                this.students.add("양시현");
+                this.students.add("김지훈");
+                this.students.add("신서아");
+                this.students.add("노도형");
+                this.students.add("허선");
+                this.students.add("노지아");
+                this.students.add("신아인");
+                this.students.add("양시은");
+                this.students.add("노석현");
+                this.students.add("손강준");
+                this.students.add("백주안");
+                this.students.add("강단우");
+                this.students.add("오나은");
+                this.students.add("오한겸");
+                this.students.add("백예준");
+                this.students.add("노은우");
+                this.students.add("손도헌");
+                this.students.add("전진우");
+                this.students.add("안주하");
+                this.students.add("백다은");
+                this.students.add("유민기");
+                this.students.add("노휘");
+                this.students.add("박채원");
+                this.students.add("강다겸");
+                this.students.add("신여준");
+                this.students.add("허은유");
+                this.students.add("심서후");
+                this.students.add("유준이");
+                this.students.add("전민승");
+                this.students.add("강예은");
+                this.students.add("허로이");
+                this.students.add("유유진");
+                this.students.add("권건하");
+                this.students.add("하도이");
+                this.students.add("노도원");
+                this.students.add("문하성");
+                this.students.add("오시후");
+                this.students.add("이로윤");
+                this.students.add("노지후");
+                this.students.add("권채이");
+                this.students.add("전담");
+                this.students.add("하태율");
+                this.students.add("노다율");
+                this.students.add("장우솔");
+                this.students.add("황하준");
+                this.students.add("조담호");
+                this.students.add("백민우");
+                this.students.add("김시진");
+                this.students.add("박유담");
+                this.students.add("이해성");
+                this.students.add("전현성");
+                this.students.add("권유겸");
+                this.students.add("강서현");
+                this.students.add("문찬율");
+                this.students.add("한도현");
+                return this.students;
+    }
+
+
+}
