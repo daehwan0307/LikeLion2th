a79cc7c075d39bf50d5e41b7a9926bba604114bd
daehwan0307 <eoghks960307@naver.com>
2022-10-05 22:21:32 +0900

    CodeUP 1020

---
+++ src/main/java/codeup/CodeUp1020.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String newS = s.replace("-","");
+        System.out.println(newS);
+
+    }
+}
