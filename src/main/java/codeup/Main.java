6bbadce3bf58f6d4bc7544a313eae2147d637ead
daehwan0307 <eoghks960307@naver.com>
2022-10-05 09:23:48 +0900

    swap ex

--- src/main/java/codeup/Main.java
+++ src/main/java/codeup/Main.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final char[][] arr = {
+                {'\u250C', '\u252C', '\u2510'},
+                {'\u251C', '\u253C', '\u2524'},
+                {'\u2514', '\u2534', '\u2518'}
+        };
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a);
+        final StringBuilder sb = new StringBuilder();
+        for(char[] cs : arr) {
+            for (char c : cs) {
+                sb.append(c);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
+
