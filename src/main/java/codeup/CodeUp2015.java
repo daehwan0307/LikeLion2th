5f58d18f5a7bdfdf361f7e04e5d74607857a1430
daehwan0307 <eoghks960307@naver.com>
2022-10-05 21:54:58 +0900

    CodeUP 1015

---
+++ src/main/java/codeup/CodeUp2015.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp2015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+        System.out.printf("%.2f", a);
+    }
+}
