f575a5a8bacdca5710a501c544573532cbfa1302
daehwan0307 <eoghks960307@naver.com>
2022-10-05 21:31:44 +0900

    codeup 1011번 문제

---
+++ src/main/java/codeup/CodeUp1011.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1011 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(x);
+    }
+}
