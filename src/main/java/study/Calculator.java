b1a0a354c69582063d91f59c593de74f0b1e4421
daehwan0307 <eoghks960307@naver.com>
2022-10-05 10:20:00 +0900

    calculator

---
+++ src/main/java/study/Calculator.java
@@ -0,0 +1,21 @@
+package study;
+
+public class Calculator {
+
+    public static int plus(int a,int b){
+
+        return a+b;
+    }
+    public static int minus(int a,int b){
+
+        return a-b;
+    }
+    public static int multiple(int a,int b){
+
+        return a*b;
+    }
+    public static int divide(int a,int b){
+
+        return a/b;
+    }
+}
---
+++ src/main/java/study/CalculatorMain.java
@@ -0,0 +1,10 @@
+package study;
+
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.plus(3,6));
+    }
+}
