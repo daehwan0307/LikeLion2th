8b19a9b286007bae3a6c7aeec384bb78f9841d9b
daehwan0307 <eoghks960307@naver.com>
2022-10-05 15:45:19 +0900

    students name 출력

---
+++ src/main/java/collection/LikeLion2th.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeLion2th {
+
+    private List<String> students = new ArrayList<>();
+
+    public LikeLion2th() {
+       Names names = new Names();
+       this.students = names.names();
+    }
+
+    //멋사 2기 학생들의 이름이 들어있는 list를 return
+    public List<String> getStudentList(){
+        return this.students;
+    }
+}
