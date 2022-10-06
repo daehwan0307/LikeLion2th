8cbca52f54ccbad306d426df57146d62ccd4aa03
daehwan0307 <eoghks960307@naver.com>
2022-10-06 10:50:37 +0900

    알파벳 생성

---
+++ src/main/java/study/day3/collection/AlphabetGenerator.java
@@ -0,0 +1,20 @@
+package study.day3.collection;
+
+import java.util.HashSet;
+
+public class AlphabetGenerator{
+
+    public static void main(String[] args) {
+
+        HashSet<Character> setChar = new HashSet<>();
+        for (int i =0; i < 100; i++) {
+            int randNum = (int)(Math.random()*26);
+            char randChar = (char) (randNum + 65);
+            setChar.add(randChar);
+        }
+
+        System.out.println(setChar);
+        System.out.println(setChar.size());
+
+    }
+}
