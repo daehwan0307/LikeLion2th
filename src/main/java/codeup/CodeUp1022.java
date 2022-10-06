1f42bbff66f7f7f36b3f8c327b18eaaa36577a95
daehwan0307 <eoghks960307@naver.com>
2022-10-06 22:54:52 +0900

    Codeup 1022

---
+++ src/main/java/codeup/CodeUp1022.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] value = sc.next().split("[.]");
+        int a = Integer.valueOf(value[0]);
+        int b = Integer.valueOf(value[1]);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
