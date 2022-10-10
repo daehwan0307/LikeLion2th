b9282bd3f4fba7467ef926e88e06b3c6f48e7ee8
daehwan0307 <eoghks960307@naver.com>
2022-10-10 23:37:03 +0900

    CodeUp 1031,1032

--- src/main/java/codeup/CodeUp1031.java
+++ src/main/java/codeup/CodeUp1031.java
@@ -6,6 +6,9 @@ public class CodeUp1031 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.printf("%o", num);
+        System.out.printf("%x", num);
+
+        //%o는 8진수
+        //%x 는 16진수로 표현
     }
 }
