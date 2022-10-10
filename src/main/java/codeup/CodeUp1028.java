5bf3c43c984c8a99a3d915e365d4e1c2840fb9e5
daehwan0307 <eoghks960307@naver.com>
2022-10-10 23:21:36 +0900

    CodeUp 1028

---
+++ src/main/java/codeup/CodeUp1028.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1028 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String arr = sc.next();
+            System.out.printf("%s",  arr);
+        }
+}
