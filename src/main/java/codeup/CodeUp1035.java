cec3d5ad001a7ce110e8dba7404e1dbaeda5497f
daehwan0307 <eoghks960307@naver.com>
2022-10-11 20:50:27 +0900

    CodeUp 1035

--- src/main/java/codeup/CodeUp1035.java
+++ src/main/java/codeup/CodeUp1035.java
@@ -2,12 +2,12 @@ package codeup;
 
 import java.util.Scanner;
 
-public class CodeUp1034 {
+public class CodeUp1035 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String num = sc.next();
 
-        int oc = Integer.parseInt(num,8);
-        System.out.printf("%d", oc);
+        int oc = Integer.parseInt(num,16);
+        System.out.printf(Integer.toOctalString(oc));
     }
 }
