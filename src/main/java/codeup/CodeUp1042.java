8427cf19724dde67ef4313915c58cd263c69d7b9
daehwan0307 <eoghks960307@naver.com>
2022-10-11 21:57:52 +0900

    CodeUp 1042

---
+++ src/main/java/codeup/CodeUp1042.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+
+        int num =  first/second;
+
+        System.out.println(num);
+    }
+}
