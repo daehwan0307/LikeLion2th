87a99102da59119e66a5ae4e625f752d6b5c8501
daehwan0307 <eoghks960307@naver.com>
2022-10-12 21:04:45 +0900

    10/12 study

--- src/main/java/study/day1012/Practice.java
+++ src/main/java/study/day1012/Practice.java
@@ -1,17 +1,28 @@
 package study.day1012;
 
+interface Operation{
+    int operation(int a, int b);
+}
 public class Practice {
 
-    public void Mul(int num){
+    public void template(int num,String operator,Operation operation){
 
         for(int i=0; i<10;i++){
-            System.out.printf("%d*%d=%d\n",num,i,num*i);
+            System.out.printf("%d %s %d = %d\n",num,operator,i,operation.operation(num,i));
         }
+    }
+    public void printPlusGugu(int num){
+
+        template(num,"+",(a,b)->a+b);
+    }
+    public void printMulGugu(int num){
+
+        template(num,"+",(a,b)->a*b);
     }
 
     public static void main(String[] args) {
         Practice practice = new Practice();
-        practice.Mul(3); //n단 출력
-        
+
+
     }
 }
