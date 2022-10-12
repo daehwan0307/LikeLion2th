1f8e58b3f84354808b0c06d2284607210649c47e
daehwan0307 <eoghks960307@naver.com>
2022-10-12 10:20:30 +0900

    CodeUp 2081

---
+++ src/main/java/codeup2/CodeUp2081.java
@@ -0,0 +1,19 @@
+package codeup2;
+
+public class CodeUp2081 {
+    public int getMax(int[] arr){
+        int maxValue = 0;
+        for(int i = 0; i<arr.length;i++){
+            if(arr[maxValue]<arr[i]) {
+                maxValue = i;
+            }
+        }
+            return maxValue;
+    };
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
