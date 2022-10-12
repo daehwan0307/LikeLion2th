9787e394b3d7d09050b22e85bf56e7e814f5f2da
daehwan0307 <eoghks960307@naver.com>
2022-10-12 23:14:49 +0900

    SWEA 2070

---
+++ src/main/java/SWEA/swea2070.java
@@ -0,0 +1,27 @@
+package SWEA;
+
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.printf("#%d ",test_case);
+            if(a>b)
+                System.out.println(">");
+            else if(a==b)
+                System.out.println("=");
+            else
+                System.out.println("<");
+        }
+    }
+}
