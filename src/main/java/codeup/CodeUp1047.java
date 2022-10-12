7789fd4e6890fca989ca60d6a8974f41d71a660b
daehwan0307 <eoghks960307@naver.com>
2022-10-12 21:38:10 +0900

    CodeUp  1047

---
+++ src/main/java/codeup/CodeUp1047.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        a = a<<1;
+        System.out.println(a);
+    }
+}
