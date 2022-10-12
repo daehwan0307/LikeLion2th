7419757d133cac2e95bc5e539d898cab429d4ac2
daehwan0307 <eoghks960307@naver.com>
2022-10-12 21:19:55 +0900

    CodeUp  1045

---
+++ src/main/java/codeup/CodeUp1045.java
@@ -0,0 +1,19 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1045 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long first = sc.nextLong();
+        long second = sc.nextLong();
+        System.out.println(first+second);
+        System.out.println(first-second);
+        System.out.println(first*second);
+        System.out.println(first/second);
+        System.out.println(first%second);
+        double A = (double) first;
+        System.out.printf("%.2f\n", A/second);
+
+    }
+}
