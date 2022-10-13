cbd999edf212177b3195197090a6c0a6e5063f20
daehwan0307 <eoghks960307@naver.com>
2022-10-13 09:45:26 +0900

    Bubble sort

--- src/main/java/study/day1013/BubbleSort01.java
+++ src/main/java/study/day1013/BubbleSort01.java
@@ -2,6 +2,8 @@ package study.day1013;
 
 import algorithm.BubbleSort;
 
+import java.util.Arrays;
+
 public class BubbleSort01 {
 
     public int[] sort(int[] arr){
@@ -16,7 +18,7 @@ public class BubbleSort01 {
                 }
 
             }
-            System.out.print(arr[i]);
+
         }
         return arr;
     }
@@ -26,6 +28,7 @@ public class BubbleSort01 {
 
         BubbleSort01 bubbleSort01 = new BubbleSort01();
         bubbleSort01.sort(arr);
+        System.out.println(Arrays.toString(arr));
 
 
     }
