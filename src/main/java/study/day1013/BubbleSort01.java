4dfb027e3d44f253f02e817e50d990e0e37409e7
daehwan0307 <eoghks960307@naver.com>
2022-10-13 09:44:04 +0900

    devide class

--- src/main/java/study/day1013/BubbleSort01.java
+++ src/main/java/study/day1013/BubbleSort01.java
@@ -1,10 +1,10 @@
 package study.day1013;
 
+import algorithm.BubbleSort;
+
 public class BubbleSort01 {
 
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{7,2,3,9,28,11};
+    public int[] sort(int[] arr){
         for(int i = 0; i<arr.length; i++){
             for(int j = i+1 ; j<arr.length; j++)
             {
@@ -13,11 +13,20 @@ public class BubbleSort01 {
                     int temp = arr[i];
                     arr[i]=arr[j];
                     arr[j]=temp;
-
                 }
 
             }
+            System.out.print(arr[i]);
         }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7,2,3,9,28,11};
+
+        BubbleSort01 bubbleSort01 = new BubbleSort01();
+        bubbleSort01.sort(arr);
+
 
     }
 
