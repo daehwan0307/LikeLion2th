40727b592a5707d1a389f24aee13f4e4d7c1cf4f
daehwan0307 <eoghks960307@naver.com>
2022-10-16 21:15:50 +0900

    CodeUp 1079

---
+++ src/main/java/codeup2/CodeUp1079.java
@@ -0,0 +1,16 @@
+package codeup2;
+
+import java.util.Scanner;
+
+public class CodeUp1079 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String a = sc.next();
+            System.out.println(a);
+            if(a.equals("q")){
+                break;
+            }
+        }
+    }
+}
