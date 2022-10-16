3c76dd8d3aff06bbe8c5148d904551f25a817db9
daehwan0307 <eoghks960307@naver.com>
2022-10-16 20:49:56 +0900

    CodeUp 1080

---
+++ src/main/java/codeup2/CodeUp1080.java
@@ -0,0 +1,20 @@
+package codeup2;
+
+import java.util.Scanner;
+
+public class CodeUp1080 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        for(int i=1; i<=num; i++){
+            sum += i;
+            if(sum >= num){
+                System.out.print(i);
+                break;
+            }
+        }
+
+    }
+}
