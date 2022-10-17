9e7b6a7409f7bf17992819ebd1c3ed1724c17b9c
daehwan0307 <eoghks960307@naver.com>
2022-10-17 09:17:28 +0900

    star

--- src/main/java/study/day1017.java
+++ src/main/java/study/day1017.java
@@ -2,9 +2,12 @@ package study;
 
 public class day1017 {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        for(int i = 0; i<4;i++){
+            for(int j= 0; j<=i;j++){
+                System.out.printf("*");
+
+            }
+            System.out.println("\n");
+        }
     }
 }
