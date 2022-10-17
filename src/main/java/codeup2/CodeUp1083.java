1cf8de00b0ff9b947611026169b88db6fe7b3eda
daehwan0307 <eoghks960307@naver.com>
2022-10-17 20:22:19 +0900

    CodeUp 1083

---
+++ src/main/java/codeup2/CodeUp1083.java
@@ -0,0 +1,19 @@
+package codeup2;
+
+import java.util.Scanner;
+
+public class CodeUp1083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            if(i%3==0){
+                System.out.printf("X ");
+
+            }else {
+                System.out.printf("%d ",i);
+            }
+        }
+    }
+}
