cfd13c74e50c240dc4be1dd48bb23ea00e720d8d
daehwan0307 <eoghks960307@naver.com>
2022-10-17 09:09:40 +0900

    star

---
+++ src/main/java/study/day1017.java
@@ -0,0 +1,10 @@
+package study;
+
+public class day1017 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
