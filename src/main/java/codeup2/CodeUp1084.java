5a721d47f231119b8dbb874bec1b04f202df7e02
daehwan0307 <eoghks960307@naver.com>
2022-10-17 22:51:15 +0900

    CodeUp 1084

---
+++ src/main/java/codeup2/CodeUp1084.java
@@ -0,0 +1,30 @@
+package codeup2;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class CodeUp1084 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] strArr = str.split(" ");
+        int a = Integer.parseInt(strArr[0]);
+        int b = Integer.parseInt(strArr[1]);
+        int c = Integer.parseInt(strArr[2]);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count=0;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                for (int k = 0; k < c; k++) {
+                    bw.write(i + " " + j + " " + k + "\n");
+                    count++;
+                }
+            }
+        }
+        bw.write(String.valueOf(count));
+        bw.flush();
+    }
+}
