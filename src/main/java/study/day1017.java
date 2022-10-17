a571172f93b5330e6037bd26b3fcedd11b9e72fb
daehwan0307 <eoghks960307@naver.com>
2022-10-17 09:30:39 +0900

    star

--- src/main/java/study/day1017.java
+++ src/main/java/study/day1017.java
@@ -4,13 +4,13 @@ public class day1017 {
     public static void main(String[] args) {
 
         day1017 day = new day1017();
-        day.makeT(5);
+        day.makeT(5,"p");
     }
 
-    public void makeT(int num){
+    public void makeT(int num,String str){
         for(int i = 0; i<num;i++){
             for(int j= 0; j<=i;j++){
-                System.out.printf("*");
+                System.out.printf(str);
             }
             System.out.println("\n");
         }
