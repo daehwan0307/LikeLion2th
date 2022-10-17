cb940fa7f1bc117ec7db6e63dc4f8567c89b3c4a
daehwan0307 <eoghks960307@naver.com>
2022-10-17 09:21:14 +0900

    star

--- src/main/java/study/day1017.java
+++ src/main/java/study/day1017.java
@@ -2,12 +2,18 @@ package study;
 
 public class day1017 {
     public static void main(String[] args) {
-        for(int i = 0; i<4;i++){
+
+        day1017 day = new day1017();
+        day.makeT(3);
+    }
+
+    public void makeT(int num){
+        for(int i = 0; i<num;i++){
             for(int j= 0; j<=i;j++){
                 System.out.printf("*");
-
             }
             System.out.println("\n");
         }
     }
 }
+
