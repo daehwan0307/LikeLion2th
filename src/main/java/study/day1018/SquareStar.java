e4cf0c3a6356051e9df0bbc2416459f190d49f18
daehwan0307 <eoghks960307@naver.com>
2022-10-18 09:21:37 +0900

    rectangle Star

--- src/main/java/study/day1018/SquareStar.java
+++ src/main/java/study/day1018/SquareStar.java
@@ -4,12 +4,20 @@ public class SquareStar {
     public static void main(String[] args) {
 
         SquareStar ss = new SquareStar();
-        ss.printSquareStar(3);
+        ss.printRecStar(3,4);
     }
 
     public void printSquareStar(int size){
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
+                System.out.printf("*");
+            }
+            System.out.println("\n");
+        }
+    }
+    public void printRecStar(int a, int b){
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
                 System.out.printf("*");
             }
             System.out.println("\n");
