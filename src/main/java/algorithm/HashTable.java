43867e28f8918cbf62cd6f40475a0ab472ec8447
daehwan0307 <eoghks960307@naver.com>
2022-10-25 09:49:39 +0900

    hash table

---
+++ src/main/java/algorithm/HashTable.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTable {
+
+    private int size = 10000;
+    private int[] table = new int[size];
+    public HashTable() {
+    }
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table=new int[size];
+    }
+
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % size;
+    }
+
+    public  void insert(String key, Integer value){
+        int hashCode =hash(key);
+        this.table[hashCode]=value;
+        System.out.println(hashCode+"방에 저장완료");
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+        HashTable ht = new HashTable();
+
+        for (int i = 0; i < names.length; i++) {
+          ht.insert(names[i],ht.hash(names[i]));
+        }
+        
+
+    }
+}
--- src/main/java/algorithm/Main.java
+++ src/main/java/algorithm/Main.java
@@ -1,27 +1,29 @@
-package algorithm;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[][] commands ={{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-        Main main = new Main();
-        main.solution(array,commands);
-        System.out.println(Arrays.toString( main.solution(array,commands)));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
-    }
+class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            String tmp = String.valueOf(i);
+            if (tmp.contains("3") || tmp.contains("6") || tmp.contains("9")) {
+                for (int j = 0; j < tmp.length(); j++) {
+                    if (tmp.charAt(j) == '3' || tmp.charAt(j) == '6' || tmp.charAt(j) == '9') {
+                        System.out.print("-");
+                    }
+                }
+                System.out.print(" ");
+            } else {
+                System.out.print(i + " ");
+            }
 
-    public int[] solution(int[] array, int[][] commands){
-        int[] answer = new int[commands.length];
 
-        for (int i = 0; i < commands.length; i++) {
-            int[] slicedArray = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
-            Arrays.sort(slicedArray);
-            answer[i]=slicedArray[commands[i][2]-1];
         }
-        return answer;
+
     }
 }
