dc7aeac0bd40e32fa80b2c87219e61a8fa0b0a04
daehwan0307 <eoghks960307@naver.com>
2022-10-27 23:46:01 +0900

    swea 2056 연월일 달력

--- src/main/java/SWEA/swea2070.java
+++ src/main/java/SWEA/swea2070.java
@@ -1,27 +1,33 @@
 package SWEA;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class swea2070 {
     public static void main(String args[]) throws Exception
     {
+        int[] dayNum = {31,28,31,30,31,30,31,31,30,31,30,31};  // 1월~12월까지 각 날짜 수
 
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            String A = sc.next();  //연원일 순으로 구성된 8자리 날짜 입력
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.printf("#%d ",test_case);
-            if(a>b)
-                System.out.println(">");
-            else if(a==b)
-                System.out.println("=");
-            else
-                System.out.println("<");
+            String Year = A.substring(0, 4);  //년도 저장
+            String Month = A.substring(4, 6); //달 저장
+            String Day = A.substring(6, 8); //일 저장
+
+            if (Integer.parseInt(Month) > 12 || Integer.parseInt(Month) <= 0) {
+                System.out.printf("#%d -1\n",i);
+            } else if (Integer.parseInt(Day) > dayNum[Integer.parseInt(Month) - 1] || Integer.parseInt(Day) <= 0) {
+                System.out.printf("#%d -1\n",i);
+            } else {
+                System.out.printf("#%d %s/%s/%s\n",i, Year, Month, Day);
+            }
+
         }
-}
+
+    }
 }
 
