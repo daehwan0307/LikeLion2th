1693fbbec91dd3856349b1ae1da2b026eed6b354
daehwan0307 <eoghks960307@naver.com>
2022-10-31 10:38:01 +0900

    모의고사 문제

--- src/test/java/study/day1028/Main.java
+++ src/test/java/study/day1028/Main.java
@@ -5,9 +5,32 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    /*
+         1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+         2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+         3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+       */
     public int[] solution(int[] answers) {
             int[] answer = {};
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        int firstAnswer = 0;
+        int secondAnswer = 0;
+        int thirdAnswer = 0;
+
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == first[i % 5]) {
+                firstAnswer++;
+            }
+            if(answers[i] == second[i % 8]) {
+                secondAnswer++;
+            }
+            if(answers[i] == third[i % 10]) {
+                thirdAnswer++;
+            }
+        }
+        int max = Math.max(Math.max(firstAnswer, secondAnswer), thirdAnswer);
             return answer;
     }
 
