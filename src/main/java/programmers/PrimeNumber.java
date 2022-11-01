af73bd248860a3b283b51910baf216c42e14f33f
daehwan0307 <eoghks960307@naver.com>
2022-11-01 10:17:44 +0900

    template적용 1단계

--- src/main/java/programmers/PrimeNumber.java
+++ src/main/java/programmers/PrimeNumber.java
@@ -5,10 +5,14 @@ public class PrimeNumber {
     //i<num
     //i<num / 2
     // i*i <num
+    boolean someOperation(int a , int b){
+        return a<b;
+    }
+    
 
     boolean isPrime(int num) {
 
-        for (int i = 2; i < num; i++) {
+        for (int i = 2; someOperation(i,num); i++) {
             if(num % i != 0) return false;
         }
         return true;
