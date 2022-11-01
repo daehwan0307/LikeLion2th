88f1b04bbbf2509d210fc4c489045b3fbbf3be46
daehwan0307 <eoghks960307@naver.com>
2022-11-01 10:01:36 +0900

    isPrime

--- src/main/java/programmers/PrimeNumber.java
+++ src/main/java/programmers/PrimeNumber.java
@@ -1,10 +1,14 @@
 package programmers;
 
 public class PrimeNumber {
-
-    public int solution(int n) {
-        int answer = 0;
-        return answer;
+    
+    boolean isPrime(int num) {
+        // num = 13이니까 12까지 나누어 봅니다.
+        for (int i = 2; i < num; i++) {
+            if(num % i != 0) return false;
+        }
+        return true;
     }
+
 
 }
