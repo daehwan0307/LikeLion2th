abc9ba2c2476fab1bc8fb4b277f57e7bde96b91e
daehwan0307 <eoghks960307@naver.com>
2022-11-07 10:20:06 +0900

    프로그래머스 하샤드 수

--- src/main/java/programmers/HarshadNumber.java
+++ src/main/java/programmers/HarshadNumber.java
@@ -1,15 +1,26 @@
 package programmers;
 
-public class HarshadNumber {
-    public boolean solution(int x) {
-        int sumOfDigit = 0;
-        while (x > 0) {
-            sumOfDigit += x % 10;
-            x = x / 10;
-        }
-        System.out.println(sumOfDigit);
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        // 자릿수 합으로 나누어 지는지
-        return false;
+public class HarshadNumber {
+    public int[] solution(int[] arr, int divisor) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) list.add(arr[i]);
+        }
+
+        if(list.size() == 0) return new int[]{-1};
+
+        // list를 Array로 바꾸고
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        // sort 한 것을 리턴
+        Arrays.sort(answer);
+        return answer;
     }
 }
