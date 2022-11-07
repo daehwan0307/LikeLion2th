f1ce3fb49c1c13dfbb4f59d259040521ad20221e
daehwan0307 <eoghks960307@naver.com>
2022-11-07 09:26:52 +0900

    프로그래머스 하샤드 수

--- src/main/java/programmers/HarshadNumber.java
+++ src/main/java/programmers/HarshadNumber.java
@@ -2,7 +2,14 @@ package programmers;
 
 public class HarshadNumber {
     public boolean solution(int x) {
-        boolean answer = true;
-        return answer;
+        int sumOfDigit = 0;
+        while (x > 0) {
+            sumOfDigit += x % 10;
+            x = x / 10;
+        }
+        System.out.println(sumOfDigit);
+
+        // 자릿수 합으로 나누어 지는지
+        return false;
     }
 }
