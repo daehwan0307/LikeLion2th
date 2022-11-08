441c018906af1bdcd06fd514a331655372ee0020
daehwan0307 <eoghks960307@naver.com>
2022-11-08 09:29:43 +0900

    CodeUp 3001

--- src/main/java/programmers/HarshadNumber.java
+++ src/main/java/programmers/HarshadNumber.java
@@ -1,26 +1,22 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class HarshadNumber {
-    public int[] solution(int[] arr, int divisor) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] numArr = {};
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) list.add(arr[i]);
+        for (int i = 0; i < size; i++) {
+            numArr[i]=sc.nextInt();
         }
+        int answer = sc.nextInt();
 
-        if(list.size() == 0) return new int[]{-1};
-
-        // list를 Array로 바꾸고
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        for (int i = 0; i < numArr.length; i++) {
+            if(answer==numArr[i])
+                System.out.printf("%d",i);
         }
-        // sort 한 것을 리턴
-        Arrays.sort(answer);
-        return answer;
     }
+
 }
