9437be792ef4071fa5117caf755c1f610a71d214
daehwan0307 <eoghks960307@naver.com>
2022-11-10 09:19:58 +0900

    programmers 같은 숫자는 싫어

--- src/main/java/programmers/HarshadNumber.java
+++ src/main/java/programmers/HarshadNumber.java
@@ -4,19 +4,26 @@ import java.util.*;
 
 public class HarshadNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] numArr = new int[size];
+        int targetNum = 7;
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        // 중간값 찾기
+        int startIdx = 0;
+        int endIdx = nums.length - 1;
+        int midIdx = nums.length / 2;
+        int midValue = nums[midIdx];
 
-        for (int i = 0; i < size; i++) {
-            numArr[i]=sc.nextInt();
-        }
-        int answer = sc.nextInt();
-
-        for (int i = 0; i < numArr.length; i++) {
-            if(answer==numArr[i])
-                System.out.printf("%d",i+1);
+        // 인덱스(시작점, 끝점) 옮기기
+        if (midValue > targetNum) { // 7이 6보다 큽니까? 작은것 모두 버림
+            startIdx = midIdx + 1;
+        } else if (midValue < targetNum) { // 6 이상을 모두 버림
+            endIdx = midIdx - 1;
+        } else {
+            // 같은경우 = 답
+            // 같은지 비교하기
+            System.out.println(midIdx);
         }
     }
+
+
 
 }
