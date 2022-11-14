a77e4c3d350c0dd9f372302e96ccddbfc013cd80
daehwan0307 <eoghks960307@naver.com>
2022-11-14 09:57:53 +0900

    에라토스테네스의 채

--- src/main/java/codeup/CodeUp2083.java
+++ src/main/java/codeup/CodeUp2083.java
@@ -3,22 +3,24 @@ package codeup;
 public class CodeUp2083 {
     public static void main(String[] args) {
         int targetNum = 7;
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        // 중간값 찾기
-        int startIdx = 0;
-        int endIdx = nums.length - 1;
-        int midIdx = nums.length / 2;
-        int midValue = nums[midIdx];
+//        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+//        // 중간값 찾기
+//        int startIdx = 0;
+//        int endIdx = nums.length - 1;
+//        int midIdx = nums.length / 2;
+//        int midValue = nums[midIdx];
+//
+//        // 인덱스(시작점, 끝점) 옮기기
+//        if (midValue > targetNum) { // 7이 6보다 큽니까? 작은것 모두 버림
+//            startIdx = midIdx + 1;
+//        } else if (midValue < targetNum) { // 6 이상을 모두 버림
+//            endIdx = midIdx - 1;
+//        } else {
+//            // 같은경우 = 답
+//            // 같은지 비교하기
+//            System.out.println(midIdx);
+//        }
+        String as = "asdf";
 
-        // 인덱스(시작점, 끝점) 옮기기
-        if (midValue > targetNum) { // 7이 6보다 큽니까? 작은것 모두 버림
-            startIdx = midIdx + 1;
-        } else if (midValue < targetNum) { // 6 이상을 모두 버림
-            endIdx = midIdx - 1;
-        } else {
-            // 같은경우 = 답
-            // 같은지 비교하기
-            System.out.println(midIdx);
-        }
     }
 }
