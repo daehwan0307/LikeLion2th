3d5320ca544bb3f63d967fe3270d2cb72a1e22a9
daehwan0307 <eoghks960307@naver.com>
2022-11-18 09:27:10 +0900

    시저 암호 진행중

--- src/main/java/study/day1021/QuickSort.java
+++ src/main/java/study/day1021/QuickSort.java
@@ -1,25 +1,32 @@
 package study.day1021;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-    public static void main(String[] args) {
-        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50}; // size = 8
 
-        // 1. 기준값 뽑는 로직 구현
-        int pivot = arr[arr.length / 2]; // 5
-        System.out.println(pivot);
-        // 2. 기준값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
+    public static void main(String[]  args) {
 
-        for (int i = 0; i < arr.length; i++) {
-            if(pivot > arr[i]) left.add(arr[i]);
-            else if (pivot < arr[i]) right.add(arr[i]);
-        }
-        System.out.println(left); //
-        System.out.println(right); // 20, 18, 19, 25, 40, 50
+    String s = "AB";
+    int n = 1;
 
+    QuickSort qs = new QuickSort();
+
+    System.out.printf(Arrays.toString(qs.solution(s,n)) );
     }
+
+    public String[] solution(String s, int n){
+        String answer = "";
+        String[] strArr = answer.split("");
+
+        for (int i = 0; i < strArr.length; i++) {
+            int ch = s.charAt(i)+n;
+           strArr[i] = Integer.toString(ch);
+        }
+        return strArr;
+    }
+
 }
+
+
