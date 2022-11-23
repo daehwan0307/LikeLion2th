823b1483cd58d84debf53c25a8dd6cbeb6ef01f4
daehwan0307 <eoghks960307@naver.com>
2022-11-23 10:48:06 +0900

    피보나치 수열

--- src/main/java/study/fac.java
+++ src/main/java/study/fac.java
@@ -2,7 +2,7 @@ package study;
 
 public class fac {
     public static void main(String[] args) {
-        
+
     }
     public static int factorial(int num) {
         if(num == 1) return num; // 1일때 1을 리턴함
---
+++ src/main/java/study/fib.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class fib {
+    public int fib(int n) {
+        if(n == 1 | n == 2) return 1;
+
+        List<Integer> fibonacciNums = new ArrayList<>();
+        fibonacciNums.add(1);
+        fibonacciNums.add(1);
+        for (int j = 2; j < n; j++) {
+            fibonacciNums.add(fibonacciNums.get(j - 1) + fibonacciNums.get(j - 2));
+        }
+
+        return fibonacciNums.get(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
