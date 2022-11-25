9c39dec2492a89a8db0c2847de5d788fa95f4be8
daehwan0307 <eoghks960307@naver.com>
2022-11-25 09:18:46 +0900

    gcd 구하기

---
+++ src/main/java/study/gcd.java
@@ -0,0 +1,14 @@
+package study;
+
+public class gcd {
+    public static int gcd(int a, int b) {
+
+        while(b!=0) {
+            int r= a%b;
+            a=b;
+            b=r;
+        }
+
+        return a;
+    }
+}
